4f2983cbd6994bd57dd2f949a9f4fe34f5c7087a
Kittidetchogun <141381533+Kittidetchogun@users.noreply.github.com>
2024-10-08 23:47:46 +0700

    getter setter topic and user

--- itutor_server/Create_Topic/topic.js
+++ itutor_server/Create_Topic/topic.js
@@ -1,9 +1,66 @@
 class Topic {
     constructor(name, description, meetingPlace, dateTime, maxParticipants) {
-        this.name = name;
-        this.description = description;
-        this.meetingPlace = meetingPlace;
-        this.dateTime = dateTime;
-        this.maxParticipants = maxParticipants;
+        setName(name);
+        setDescription(description);
+        setMeetingPlace(meetingPlace);
+        setDateTime(dateTime);
+        setMaxParticipants(maxParticipants);
+    }
+
+    // Getter methods (using arrow functions for conciseness)
+    get name() {
+        return this.name;
+    }
+
+    get description() {
+        return this.description;
+    }
+
+    get meetingPlace() {
+        return this.meetingPlace;
+    }
+
+    get dateTime() {
+        return this.dateTime;
+    }
+
+    get maxParticipants() {
+        return this.maxParticipants;
+    }
+
+    // Setter methods (using traditional functions for clarity)
+    setName(newName) {
+        if (typeof newName !== 'string') {
+        throw new TypeError('Topic name must be a string.');
+        }
+        this.name = newName;
+    }
+
+    setDescription(newDescription) {
+        if (typeof newDescription !== 'string') {
+        throw new TypeError('Topic description must be a string.');
+        }
+        this.description = newDescription;
+    }
+
+    setMeetingPlace(newMeetingPlace) {
+        if (typeof newMeetingPlace !== 'string') {  // Ensure meetingPlace is a string
+        throw new TypeError('Topic meeting place must be a string.');
+        }
+        this.meetingPlace = newMeetingPlace;
+    }
+
+    setDateTime(newDateTime) {
+        if (!(newDateTime instanceof Date)) {
+        throw new TypeError('Topic dateTime must be a Date object.');
+        }
+        this.dateTime = newDateTime;
+    }
+
+    setMaxParticipants(newMaxParticipants) {
+        if (typeof newMaxParticipants !== 'number' || newMaxParticipants <= 0) {
+        throw new TypeError('Topic maxParticipants must be a positive number.');
+        }
+        this.maxParticipants = newMaxParticipants;
     }
 }
--- itutor_server/Profile_Management/user.js
+++ itutor_server/Profile_Management/user.js
@@ -1,8 +1,58 @@
 class User {
     constructor(id, username, email, profilePicture) {
-        this.id = id;
-        this.username = username;
-        this.email = email;
-        this.profilePicture = profilePicture;
+        setid(id);
+        setusername(username);
+        setemail(email);
+        setprofilePicture(profilePicture);
+    }
+
+    // Getter methods
+    get id() {
+        return this.id;
+    }
+
+    get username() {
+        return this.username;
+    }
+
+    get email() {
+        return this.email;
+    }
+
+    get profilePicture() {
+        return this.profilePicture;
+    }
+
+    // Setter methods
+    setid(newId) {
+        // เพิ่มการตรวจสอบค่า เช่น ตรวจสอบความยาวของ username
+        if (typeof newId !== 'number' || newId <= 0) {
+        throw new Error('id must be a positive number.');
+        }
+        this.id = newId;
+    }
+
+    setusername(newUsername) {
+        // เพิ่มการตรวจสอบค่า เช่น ตรวจสอบความยาวของ username
+        if (typeof newUsername !== 'string' || newUsername.length < 3) {
+        throw new Error('Username must be a string and at least 3 characters long.');
+        }
+        this.username = newUsername;
+    }
+
+    setemail(newEmail) {
+        // ตรวจสอบรูปแบบของ email
+        if (!/^\S+@\S+\.\S+$/.test(newEmail)) {
+          throw new Error('Invalid email format.');
+        }
+        this.email = newEmail;
+    }
+
+    setprofilePicture(newProfilePicture) {
+        // ตรวจสอบว่าเป็น URL หรือไม่
+        if (!/^(http:\/\/|https:\/\/)/.test(newProfilePicture)) {
+          throw new Error('Profile picture must be a URL.');
+        }
+        this.profilePicture = newProfilePicture;
     }
 }
