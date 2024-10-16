a2648bcf9bd8bb4daf8aab14adb1d19de583b06c
FahTw <66070021@kmitl.ac.th>
2024-10-16 22:47:50 +0700

    topic ui

--- itutor_project/app/test/page.js
+++ itutor_project/app/test/page.js
@@ -26,19 +26,46 @@ export default function test() {
     }
     search();
   }, []);
+ 
   return (
-    <div>
+    <div className="h-screen">
+      <div className="bg-orange-500 flex justify-between items-center p-8">
+        <div className="logo">
+          <img className="h-14" src="/nav-bar.svg" alt="logo" />
+        </div>
+      </div>
+
+      {/*รับมาจากที่ Tutor สร้างไว้*/}
       {getTopic.map((topic) => (
-        <div className="p-4" key={topic.course_id}>
-          <h2 className="text-xl font-semibold text-center">
-            Topic Name: {topic.course_name}
-          </h2>
-          <p className="text-gray-700 text-center">
-            Topic Description: {topic.course_desc}
-          </p>
-          <p className='text-gray-700 text-center underline'>
-            OTHER COLUMN GET FROM DB COLUMN NAME THIS IS JUST EXAMPLE
-          </p>
+        <div key={topic.course_id}>
+          {/*รับมาจากที่ Tutor สร้างไว้*/}
+          <div className="my-11 text-center">
+            <h1 className="text-4xl bold">{topic.course_name}</h1>
+            <h2 className="text-xl mt-4">Limited to 20 seats</h2>
+            <h2 className="text-xl mt-4">Creative and Ideation Fl.2</h2>
+          </div>
+
+          <div className="mt-6 px-4 py-2 flex justify-center">
+            <p className="w-[140vh] break-words">{topic.course_desc}</p>
+          </div>
+
+          <div className="flex justify-center">
+            <div className="bg-[#E4EDF5] p-4 w-[140vh] rounded-lg flex justify-between items-center">
+              <div className="flex items-center space-x-4">
+                <div className="bg-gray-400 w-16 h-16 rounded-full"></div>
+                <div>
+                  <p className="font-bold">Name Surname</p>
+                  <p className="text-gray-500">@ITLADKRABANG</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-14 flex justify-center items-center">
+            <a href="">
+              <button className="w-40 p-2 rounded-[30px] bg-[#f8573c]">JOIN</button>
+            </a>
+          </div>
         </div>
       ))}
     </div>
